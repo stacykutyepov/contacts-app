eec085b4444edd602ae0383f0ee6209b84d62f84
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-16 15:45:42 -0400

    add styles to pages

---
+++ src/pages/pages.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const pagesStyles = makeStyles({
+    container: {
+        margin: "40px 0px",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+});
