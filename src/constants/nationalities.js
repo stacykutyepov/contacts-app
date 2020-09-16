8ae80ad42d175b209a968dbf45271abbf850ba53
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-16 15:45:16 -0400

    nationalities constant

---
+++ src/constants/nationalities.js
@@ -0,0 +1,24 @@
+/**
+ * https://www.borovic.ru/codes.html
+ * https://preply.com/blog/2015/07/28/nazvaniya-stran-i-natsionalnostej-na-anglijskom
+ * https://skyeng.ru/articles/strana-yazyk-natsionalnost
+ * */
+export const NATIONALITIES = {
+    AU: { name: 'Australian', color: 'magenta' },
+    BR: { name: 'Brazilian', color: 'red' },
+    CA: { name: 'Canadian', color: '#4E2728' },
+    CH: { name: 'Swiss', color: 'orange' },
+    DE: { name: 'German', color: 'gold' },
+    DK: { name: 'Danish', color: '#bfff00' },
+    ES: { name: 'Spanish', color: 'green' },
+    FI: { name: 'Finnish', color: '#00FFFF' },
+    FR: { name: 'French', color: '#0000ff' },
+    GB: { name: 'British', color: '#5F9EA0' },
+    IE: { name: 'Irish', color: '#800080' },
+    IR: { name: 'Iranian', color: '#f50' },
+    NO: { name: 'Norwegian', color: '#2db7f5' },
+    NL: { name: 'Dutch', color: '#87d068' },
+    NZ: { name: 'New Zealander', color: '#9e1068' },
+    TR: { name: 'Turkish', color: '#ffd666' },
+    US: { name: 'American', color: '#7fffd4' },
+};
