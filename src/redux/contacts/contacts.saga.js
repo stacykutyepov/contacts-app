8f03b9016493ca9ff985fd32b3bbcbf7dc7d55fc
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-16 15:44:22 -0400

    use random number in saga

--- src/redux/contacts/contacts.saga.js
+++ src/redux/contacts/contacts.saga.js
@@ -1,12 +1,12 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { setContactsFailure, setContactsSuccess } from './contacts.actions';
+import { randomIntegerInRange } from '../../utils/random'
 
 import ContactsTypes from './contacts.types';
 
 export function* fetchContactsAsync() {
-    console.log('I am fired')
     try {
-        const result = yield fetch('https://randomuser.me/api/?results=500')
+        const result = yield fetch(`https://randomuser.me/api/?results=${randomIntegerInRange(100, 1000)}`)
         const data = yield result.json()
         yield put(setContactsSuccess(data))
 
