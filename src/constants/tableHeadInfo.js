62b8c31895010cf477cebf132521c73836792f45
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-21 10:44:12 -0400

    info for tableHead

---
+++ src/constants/tableHeadInfo.js
@@ -0,0 +1,30 @@
+export const tableHeadInfo = [
+    {
+        name: "Avatar",
+        align: "left",
+    },
+    {
+        name: "Full Name",
+        align: "left",
+    },
+    {
+        name: "Birthday",
+        align: "left",
+    },
+    {
+        name: "Email",
+        align: "left",
+    },
+    {
+        name: "Phone",
+        align: "left",
+    },
+    {
+        name: "Location",
+        align: "left",
+    },
+    {
+        name: "Nationality",
+        align: "right",
+    },
+];
